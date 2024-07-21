2099c27e16dbed8c9156252c54f0f650ca5a2ea6
AyoubAnej <alyapani9@gmail.com>
2024-07-21 21:07:02 +0100

    add register and login functions (api.js)

---
+++ src/redux/auth/Action.js
@@ -0,0 +1,34 @@
+import { BASE_API_URI } from "../../config/api";
+import { LOGIN, REGISTER } from "./ActionType";
+
+export const register = (data) => async (dispatch) => {
+  try {
+    const res = await fetch(`${BASE_API_URI}/auth/signup`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const resData = await res.json();
+    console.log("register", resData);
+    dispatch({ type: REGISTER, payload: resData });
+  } catch (error) {}
+};
+
+export const login = (data) => async (dispatch) => {
+  try {
+    const res = await fetch(`${BASE_API_URI}/auth/signin`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+    const resData = await res.json();
+    console.log("register", resData);
+    dispatch({ type: LOGIN, payload: resData });
+  } catch (error) {
+    console.log("catch error ",error);
+  }
+};
